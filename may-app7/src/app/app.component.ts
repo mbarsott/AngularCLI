12a3201ef612a21ae36e6dc1ca1132649033c8af
Marcelo Barsotti <marcelo.barsotti@assentcompliance.com>
2019-09-04 17:30:37 -0400

    4. Generating a New Angular Application

---
+++ may-app7/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mfb-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'may-app7';
+}
